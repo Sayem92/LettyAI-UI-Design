4024458dfe8fb5fd85a318ca5fcdf701cd08b9f3
Sayem92 <sayemmiha123@gmail.com>
2023-01-20 12:08:09 +0600

    navbar done with resposive behaviour

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-[1440px] mx-auto dark:text-white bg-[#1E1E1E] dark:bg-[#1E1E1E] font-nunito">
+      <Home></Home>
     </div>
   );
 }
