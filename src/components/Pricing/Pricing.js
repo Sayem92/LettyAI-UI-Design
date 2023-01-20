8145a378949953f9e1c5a3e1dbc03290329e7341
Sayem92 <sayemmiha123@gmail.com>
2023-01-20 19:32:23 +0600

    add pricing section

---
+++ src/components/Pricing/Pricing.js
@@ -0,0 +1,101 @@
+import React from 'react';
+
+const Pricing = () => {
+    return (
+        <section className="py-20 text-gray-100">
+        <div className="container px-4 mx-auto">
+            <div className="max-w-2xl mx-auto mb-16 text-center">
+                <h2 className="text-4xl font-bold mb-10">PRICING</h2>
+                <p className="text-2xl font-nunito text-gray-300">Access all that Letty Al has to offer for next to nothing</p>
+              
+            </div>
+            <div className="flex  justify-center flex-wrap gap-10 ">
+            <div className="flex flex-col space-y-6 rounded shadow  bg-gray-900">
+                        <div className="space-y-2 text-center px-6 pt-6">
+                            <h4 className="text-2xl font-nunito mb-10">Letty - Full Access</h4>
+                            <span className="text-2xl font-nunito ">$100/yr</span>
+                        </div>
+                        <ul className="flex-1 mb-6 text-gray-200 px-6">
+                            <li className="flex mb-2 space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-violet-400">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                </svg>
+                                <span>Unlimited Text Generation</span>                             
+
+                            </li>
+                            <li className="flex mb-2 space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-violet-400">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                </svg>
+                                <span>Plagiarism Free Content</span>
+                            </li>
+                            <li className="flex mb-2 space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-violet-400">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                </svg>
+                                <span>Article Creation</span>
+                            </li>
+                            <li className="flex mb-2 space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-violet-400">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                </svg>
+                                <span>SEO Optimization</span>
+                            </li>
+                            <li className="flex mb-2 space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-violet-400">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                </svg>
+                                <span>Countless Use-Cases</span>
+                            </li>
+                        </ul>
+                        <button type="button" className="inline-block px-5 py-3 font-nunito tracking-wider text-center rounded bg-[#6E3FF2] text-[#FFFFFF]">BUY NOW</button>
+                    </div>
+
+                    <div className="flex flex-col  space-y-6 rounded shadow  bg-gray-900">
+                        <div className="space-y-2 text-center px-6 pt-6">
+                            <h4 className="text-2xl font-nunito mb-10">Letty - Full Access</h4>
+                            <span className="text-2xl font-nunito ">$100/yr</span>
+                        </div>
+                        <ul className="flex-1 mb-6 text-gray-200 px-6">
+                            <li className="flex mb-2 space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-violet-400">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                </svg>
+                                <span>Unlimited Text Generation</span>                             
+
+                            </li>
+                            <li className="flex mb-2 space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-violet-400">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                </svg>
+                                <span>Plagiarism Free Content</span>
+                            </li>
+                            <li className="flex mb-2 space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-violet-400">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                </svg>
+                                <span>Article Creation</span>
+                            </li>
+                            <li className="flex mb-2 space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-violet-400">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                </svg>
+                                <span>SEO Optimization</span>
+                            </li>
+                            <li className="flex mb-2 space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-violet-400">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                                </svg>
+                                <span>Countless Use-Cases</span>
+                            </li>
+                        </ul>
+                        <button type="button" className="inline-block px-5 py-3 font-nunito tracking-wider text-center rounded bg-[#6E3FF2] text-[#FFFFFF]">BUY NOW</button>
+                    </div>
+             
+            </div>
+        </div>
+    </section>
+    );
+};
+
+export default Pricing;
